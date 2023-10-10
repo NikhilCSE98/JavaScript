08bcd4f58b49a647082e3b5873326b9f6aeb0c27
NikhilCSE98 <107809463+NikhilCSE98@users.noreply.github.com>
2023-10-10 23:12:32 +0530

    Add files via upload

---
+++ BulbOnOff/Bulb.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","bulbOn.png");
+}
+function bulbOff(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","bulbOff.png");
+}
